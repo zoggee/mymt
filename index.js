306b3b9ef3be928b604d6583563193fe76af7d55
zoggee <woachxq@163.com>
2019-10-06 23:05:33 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+import Koa from 'koa'
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-generic-session'
+import redisStore from 'koa-redis'
+import json from 'koa-json'
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
+
+const app = new Koa()
+
+app.keys = ['mt', 'keyskeys']
+// app.proxy = true 
+app.use(session({
+  key: 'mt',
+  prefix: 'mt:uid',
+  store: redisStore({
+    host: 'my server's ip',
+    port: 6379
+  })
+}))
+app.use(bodyParser({
+  extendTypes: ['json', 'form', 'text']
+}))
+app.use(json())
+//连接数据库
+mongoose.connect(dbConfig.dbs, {
+  useNewUrlParser: true
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = app.env !== 'production'
+
+async function start () {
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
+
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+
+  //my code
+  app.use(users.routes())
+  app.use(users.allowedMethods())
+
+  app.use((ctx) => {
+    ctx.status = 200
+    ctx.respond = false // Bypass Koa's built-in response handling
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx.req, ctx.res)
+  })
+
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+
+start()
